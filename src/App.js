2a711fc6a649a9b3fad8f98e49faf0875f2a9f63
ltlely <lylytr.311@gmail.com>
2024-10-15 19:22:24 -0400

    changed endpoint

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
     };
    
     try {
-      const response = await fetch('http://localhost:3001/login', {
+      const response = await fetch('https://ltlely.github.io/Mathropolis/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
